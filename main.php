630aadc4a33e7a2320130376155b167e125ad969
txtb0kwrm <txtbokwrm@gmail.com>
2018-03-21 15:35:44 +0900

    Third commit.

--- main.php
+++ main.php
@@ -1,4 +1,5 @@
 <?php
+// RAM16GBの整備済みMac Book Proが追加されたらメールを送信するスクリプト
 
 require_once("lib/phpQuery-onefile.php");
 require_once("lib/PHPMailer.php");
@@ -7,7 +8,7 @@ require_once("lib/SMTP.php");
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
-// スクレイピング
+// 整備済み製品ページから情報を取得
 $url = 'https://www.apple.com/jp/shop/browse/home/specialdeals/mac/macbook_pro/13';
 $html = file_get_contents($url);
 $dom = phpQuery::newDocument($html);
@@ -21,17 +22,17 @@ if(strpos($text, '16GB 2,133MHz LPDDR3') !== false) {
 }
 
 // メール作成
-$subject = "";
-$fromname = "あらい";
-$from = "txtbowkrm@gmail.com";
-$smtp_user = "txtbokwrm@gmail.com";
-$smtp_password = "pare3gmail";
+$subject = "sendmail-16mbp";
+$fromname = "";
+$from = "";
+$smtp_user = "";
+$smtp_password = "";
 
-$to = 'youtopia03@gmail.com';
+$to = "";
 
 $mail = new PHPMailer();
 $mail->IsSMTP();
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPDebug = 0;
 $mail->SMTPAuth = true;
 $mail->CharSet = 'utf-8';
 $mail->SMTPSecure = 'tls';
@@ -46,6 +47,7 @@ $mail->Subject = $subject;
 $mail->Body = $body;
 $mail->AddAddress($to);
 
+// メール送信
 if( !$mail -> Send() ){
     $message  = "Message was not sent<br/ >";
     $message .= "Mailer Error: " . $mailer->ErrorInfo;
@@ -54,10 +56,5 @@ if( !$mail -> Send() ){
 }
 
 echo $message;
-
-
-
-
-
 
 ?>
